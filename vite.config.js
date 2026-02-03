ba8df45f7ee7c0b064db9f8f208acda4e2c724a4
myeolseoulkorea <clubiykyk@gmail.com>
2026-02-03 11:26:09 +0900

    Rename to rd-audit-demo

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/rd-defense-game/',
+  base: '/rd-audit-demo/',
 })
